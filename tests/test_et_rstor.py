09424e7560a44d14d94f4898fc1e021c11af2ec3
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2021-05-19 17:08:22 +0200

    All micc2 tutorials have been entered as test methods.

--- tests/test_et_rstor.py
+++ tests/test_et_rstor.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""Tests for et_rstor package."""
+"""Tests for et_rstor package.
+
+This is not really a collections of tests. The test methods construct the tutorials
+of micc2 which are inspected visually, not automatically.
+"""
 
 from pathlib import Path
 import re
@@ -32,7 +36,14 @@ def test_TextWrapper():
     tw.wrap(text)
 
 
-write = True
+_write = True
+def process(doc):
+    doc.verbose = True
+    if _write:
+        doc.write(Path.home() / 'workspace/et-micc2/tutorials/')
+    else:
+        print(f'$$$$$$\n{doc}\n$$$$$$')
+
 workspace = Path.home() / 'software/dev/workspace/Tutorials'
 snippets = Path(__file__).parent / '../snippets'
 
@@ -1713,10 +1724,14 @@ def test_TutorialProject_et_dot_1():
     Image('../tutorials/im-building.png')
     Paragraph(
         "There is a difference in how f2py_ and pybind11_ operate. F2py_ is an *executable* "
-        "that inspects the Fortran source and create wrappers for the subprograms it finds and "
-        "uses the compiler to build the extension module. (The wrappers are in C, so f2py_ needs "
-        "a C compiler as well). Pybind11_ is a *C++ template library* that is used to express "
-        "the interface between Python and C++."
+        "that inspects the Fortran source code and creates wrappers for the subprograms it finds. "
+        "These wrappers are C code, compiled and linked with the compiled Fortran code to build "
+        "the extension module. Thus, f2py_ needs a Fortran compiler, as well as a C compiler. "
+        "The Pybind11_ approach is conceptually simpler. Pybind11_is a *C++ template library* "
+        "that the programmer uses to express the interface between Python and C++. In fact the "
+        "introspection is done by the programmer, and there is only one compiler round, using a "
+        "C++ compiler. This gives the programmer more flexibility and control, but also a bit "
+        "more work."
     )
 
     Heading('Choosing between Fortran and C++ for binary extension modules', level=4, crosslink='f90-or-cpp')
@@ -2437,11 +2452,8 @@ def test_TutorialProject_et_dot_4():
         "the Python variable :file:`sum`, which, as said, may be prohibitively "
         "expensive."
     )
-    doc.verbose = True
-    if write:
-        doc.write(Path.home()/'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+
+    process(doc)
 
 def test_TutorialProject_et_dot_5():
     doc = RstDocument('TutorialProject_et_dot_5', headings_numbered_from_level=2, is_default_document=True)
@@ -2489,11 +2501,7 @@ def test_TutorialProject_et_dot_5():
     """
     
     """
-    doc.verbose = True
-    if write:
-        doc.write(Path.home()/'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
 
 def test_TutorialProject_et_dot_6():
     doc = RstDocument('TutorialProject_et_dot_6', headings_numbered_from_level=2, is_default_document=True)
@@ -2520,11 +2528,7 @@ def test_TutorialProject_et_dot_6():
         , language='bash', execute=True, cwd=project_path
     )
 
-    doc.verbose = True
-    if write:
-        doc.write(Path.home()/'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
 
 
 def test_TutorialProject_et_dot_7():
@@ -2624,11 +2628,7 @@ def test_TutorialProject_et_dot_7():
         , execute=True, cwd=project_path, hide=True
     )
 
-    doc.verbose = True
-    if write:
-        doc.write(Path.home()/'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
 
 def test_TutorialProject_et_dot_8():
     doc = RstDocument('TutorialProject_et_dot_8', headings_numbered_from_level=2, is_default_document=True)
@@ -2825,11 +2825,7 @@ def test_TutorialProject_et_dot_8():
     #     , execute=True, cwd=project_path
     #     , hide=True
     # )
-    doc.verbose = True
-    if write:
-        doc.write(Path.home() / 'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
 
 
 def test_TutorialVCS():
@@ -2931,11 +2927,7 @@ def test_TutorialVCS():
           "unless it is in a separate branch and you are the only one working on it. "
         ]
     )
-    doc.verbose = True
-    if write:
-        doc.write(Path.home() / 'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
 
 
 def test_TutorialVersionManagement():
@@ -3011,12 +3003,121 @@ def test_TutorialVersionManagement():
         "with name ``v<version_string>`` (see https://git-scm.com/book/en/v2/Git-Basics-Tagging) "
         "and pushes the tag to the remote repository."
     )
-    """"""
-    doc.verbose = True
-    if write:
-        doc.write(Path.home()/'workspace/et-micc2/tutorials/')
-    else:
-        print(f'$$$$$$\n{doc}\n$$$$$$')
+    process(doc)
+
+
+def test_TutorialPublish():
+
+    wsfoo = workspace / 'foo'
+    if (wsfoo).exists():
+        shutil.rmtree(wsfoo)
+
+    doc = RstDocument('TutorialPublish', headings_numbered_from_level=2, is_default_document=True)
+    doc.heading_numbers[2] = 5
+
+    Include('../HYPERLINKS.rst')
+
+    Heading('Publishing your code', level=3, crosslink='publishing')
+
+    Paragraph(
+        "By publising your code other users can easily reuse your code. Although a "
+        "public GitHub_ repository makes that possible, Python provides the Python "
+        "Package Index (PyPI_). Packages published on PyPI_ can be installed by "
+        "anyone using pip_."
+    )
+
+    Heading('Publishing to the Python Package Index', level=4, crosslink='publish-pypi')
+
+    Paragraph(
+        "Poetry_ provides really easy interface to publishing your code to the Python "
+        "Package Index (PyPI_). To install poetry see https://python-poetry.org/docs/#installation. "
+        "You must also create a PyPI_ account `here <https://pypi.org/account/register/>`_. Then"
+        "to publish the :file:`ET-dot` package, run this command in the project directory:"
+        "this command in the project directory, of:"
+    )
+    CodeBlock(
+        [ "> poetry publish --build"
+        , "Creating virtualenv et-dot in /Users/etijskens/software/dev/workspace/Tutorials/ET-dot/.venv"
+        , "Building ET-dot (0.0.1)"
+        , "  - Building sdist"
+        , "  - Built ET-dot-0.0.1.tar.gz"
+        , "  - Building wheel"
+        , "  - Built ET_dot-0.0.1-py3-none-any.whl"
+        , ""
+        , "Publishing ET-dot (0.0.1) to PyPI"
+        , " - Uploading ET-dot-0.0.1.tar.gz 100%"
+        , " - Uploading ET_dot-0.0.1-py3-none-any.whl 100%"
+        ]
+        , language='bash', prompt=''
+    )
+    Paragraph(
+        "In order for your project to be publishable, it is necessary that the project "
+        "name is not already in use on PyPI_. As there are 100s of projects on PyPI_, "
+        "it is wise to check that. You can do this manually, but micc2_ also provides a "
+        "``--publish`` flag for the ``micc2 create`` command that verifies that the "
+        "project name is still available on PyPI_. If the name is already taken, the "
+        "project will not be created and micc2_ will suggest to choose another project "
+        "name. See :ref:`project-name` for recommendations of how to choose project "
+        "names. If the name is not yet taken, it is wise to publish the freshly created "
+        "project right away (even without any useful contents), to make sure that no one "
+        "else can publish a project with the same name."
+        ""
+    )
+    Paragraph(
+        "Note that a single version of a project can only be published once. If the "
+        ":file:`ET-dot` must be modified, e.g. to fix a bug, one must bump the version "
+        "number before it can be published again. Once a version is published it cannot "
+        "be modified."
+    )
+    Warning(
+        "It is recommended to publish code from a personal computer, and not from "
+        "a cluster."
+    )
+    Paragraph(
+        "After the project is published, everyone can install the package in his "
+        "current Python environment as:"
+    )
+    CodeBlock(
+        [ "> pip install et-foo"
+        , "..."
+        ]
+        , language='bash', prompt=''
+    )
+
+    Heading('Publishing packages with binary extension modules', level=4)
+    Paragraph(
+        "Packages with binary extension modules are published in exactly the same "
+        "way. That is, perhaps surprisingly, as a Python-only project. When you "
+        "``pip install`` a Micc2_ project, the package directory will end up in the "
+        ":file:`site-packages` directory of the Python environment in which you "
+        "install. The source code directories of the binary extensions modules are "
+        "also installed with the package, but without the binary extensions themselves. "
+        "These must be compiled locally. Micc2_ has added some machinery to automatically "
+        "build the binary extensions from the source code, as explained in detail at the "
+        "end of section :ref:`building-f90`. Obviously, this 'auto-build', can only "
+        "succeed if the necessary tools are available. In case of failure because of "
+        "missing tools, micc2_ will tell you which tools are missing."
+    )
+
+    Heading('Publishing your documentation on readthedocs.org', level=4)
+
+    Paragraph(
+        "Publishing your documentation to `Readthedocs <https://readthedocs.org>`_ "
+        "relieves the users of your code from having to build documentation themselves. "
+        "Making it happen is very easy. First, make sure the git repository of your code "
+        "is pushed on Github_. Second, create a Readthedocs_ account if you do not already "
+        "have one. Then, go to your Readthedocs_ page, go to *your projects* and hit import "
+        "project. After filling in the fields, the documentation will be rebuild and published "
+        "automatically every time you push your code to the Github_ remote repository."
+    )
+    Note(
+        "Sphinx must be able to import your project in order to extract the documentation. "
+        "If your codes depend on Python modules other than the standard library, this will "
+        "fail and the documentation will not be built. You can add the necessary dependencies "
+        "to :file:`<your-project>/docs/requirements.txt`."
+    )
+
+    process(doc)
 
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
@@ -3031,7 +3132,7 @@ if __name__ == "__main__":
         tutorial = sys.argv[1]
         the_test_you_want_to_debug = eval(f'test_{tutorial}')
     else:
-        the_test_you_want_to_debug = test_TutorialVersionManagement
+        the_test_you_want_to_debug = test_TutorialPublish
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
